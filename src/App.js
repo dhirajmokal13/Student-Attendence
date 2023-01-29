7c220557828d0235db672746da5374b9d19044b9
Dhiraj <dhirajmokal13@gmail.com>
2023-01-29 14:27:39 +0530

    Changes of features and style on 29 jan

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import moment from 'moment/moment';
 
 function App() {
   const [students, setStudents] = useState([]);
@@ -10,8 +11,6 @@ function App() {
       //Some Defaults Studens
       { rn: 1, name: 'Dhiraj Mokal' },
       { rn: 2, name: 'Aniket Kolhe' },
-      { rn: 3, name: 'Sanket Gaikwad' },
-
     ]);
   }, []);
 
@@ -35,31 +34,37 @@ function App() {
   };
 
   const handleSubmit = (event) => {
-    event.preventDefault();
-    setStudents([...students, newStudent]);
+    if (newStudent.rn > students.length) {
+      event.preventDefault();
+      setStudents([...students, newStudent]);
+    } else {
+      alert(`Please Enter Roll No More Than ${students.length}`);
+    }
   };
 
   return (
     <div className="App container">
       <h2 className='text-center text-success mt-4 mb-3'>Student Attendence</h2>
-      <form onSubmit={handleSubmit}>
-        <input className='form-control my-2' type="number" placeholder={`Enter Roll Number Greater Than ${students.length}`} name="rn" value={newStudent.rn} onChange={handleChange} />
-        <input className='form-control my-2' type="text" placeholder='Enter Roll Number' name="name" value={newStudent.name} onChange={handleChange} />
+      <form onSubmit={handleSubmit} className='mx-auto mb-3' style={{ maxWidth: '30vw' }}>
+        <input className='form-control my-2' type="number" placeholder={`Enter Roll Number More Than ${students.length}`} name="rn" value={newStudent.rn} onChange={handleChange} />
+        <input className='form-control my-2' type="text" placeholder='Enter Student Name' name="name" value={newStudent.name} onChange={handleChange} />
         <button className='btn btn-outline-primary mt-2 me-2' type="submit">Submit</button>
       </form>
 
       <h5 className='text-center'>Present Students: <span className='text-danger'>{Object.keys(checkinTime).length}</span></h5>
       {students.map((student) => (
-        <div className='card my-3' key={student.rn}>
+        <div className='card my-3' style={{ boxShadow: '0 10px 16px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0,0,0,0.19)' }} key={student.rn}>
           <div className='card-body'>
             <h4 className='card-title'>Name: <span className='text-success'>{student.name}</span></h4>
+            <hr />
             <h5 className='card-text'>Roll Number: {student.rn}</h5>
             <div className="d-flex">
-              <p className='me-1'><span className='text-primary'>Checkin Time: </span>{checkinTime[student.rn] ? checkinTime[student.rn].toString() : 'Not checked in'}</p>
-              <p className='ms-1'><span className='text-primary'>Checkout Time: </span>{checkoutTime[student.rn] ? checkoutTime[student.rn].toString() : 'Not checked out'}</p>
+              <p className='me-1 text-secondary'><span className='text-primary'>Checkin Time: </span>{checkinTime[student.rn] ? moment(new Date(checkinTime[student.rn])).format('MMMM Do YYYY, h:mm a') : 'Not checked in'}</p>
+              <p className='ms-1 text-secondary'><span className='text-primary'>Checkout Time: </span>{checkoutTime[student.rn] ? moment(new Date(checkoutTime[student.rn])).format('MMMM Do YYYY, h:mm a') : 'Not checked out'}</p>
             </div>
-            <button className='btn btn-outline-primary me-2' onClick={() => checkIn(student.rn)} disabled={checkinTime[student.rn]}>Check In</button>
-            <button className='btn btn-outline-primary' onClick={() => checkOut(student.rn)} disabled={!checkinTime[student.rn] || checkoutTime[student.rn]}>Check Out</button>
+            <hr />
+            <button className='btn btn-outline-success me-2' onClick={() => checkIn(student.rn)} disabled={checkinTime[student.rn]}>Check In</button>
+            <button className='btn btn-outline-danger' onClick={() => checkOut(student.rn)} disabled={!checkinTime[student.rn] || checkoutTime[student.rn]}>Check Out</button>
           </div>
         </div>
       ))}
